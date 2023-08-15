66671367f5f8e908a769921f699bdacdf3382259
dongkyunkim <ddkds66@gmail.com>
2023-08-15 23:05:32 +0900

    service module들에 mapstruct 의존성 추가

---
+++ product-service/src/main/java/com/playground/productservice/util/mapper/config/UnmappedIgnoreConfig.java
@@ -0,0 +1,8 @@
+package com.playground.productservice.util.mapper.config;
+
+import org.mapstruct.MapperConfig;
+import org.mapstruct.ReportingPolicy;
+
+@MapperConfig(unmappedSourcePolicy = ReportingPolicy.IGNORE, unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface UnmappedIgnoreConfig {
+}

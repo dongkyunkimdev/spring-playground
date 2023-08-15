ee868f961bda37c61d42822c264cb9acb77941da
dongkyunkim <ddkds66@gmail.com>
2023-08-15 22:17:50 +0900

    service module들에 mysql connector, data jpa 의존성 및 entity basePackages 지정

---
+++ product-service/src/main/java/com/playground/productservice/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package com.playground.productservice.config;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EntityScan(basePackages = "com.playground.core.domain")
+public class JpaConfig {
+}

c09aec44762c1786c956d76b02e2cbd4fe0d43c3
dongkyunkim <ddkds66@gmail.com>
2023-08-15 21:34:52 +0900

    Business Logic 수행중에만 발생하는 BusinessException 클래스 추가

---
+++ core/src/main/java/com/playground/core/common/exception/BusinessException.java
@@ -0,0 +1,13 @@
+package com.playground.core.common.exception;
+
+public class BusinessException extends RuntimeException {
+
+    public BusinessException(String message) {
+        super(message);
+    }
+
+    public BusinessException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}

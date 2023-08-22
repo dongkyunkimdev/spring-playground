dd6540526f69d4406e42fe838f54282864d1d32c
dongkyunkim <ddkds66@gmail.com>
2023-08-22 23:48:02 +0900

    core-web 모듈 제거하고 core 모듈로 통합 및 Querydsl 의존성 추가

--- core/src/main/java/com/playground/core/config/JpaConfig.java
+++ core/src/main/java/com/playground/core/config/JpaConfig.java
@@ -1,4 +1,4 @@
-package com.playground.deliveryservice.config;
+package com.playground.core.config;
 
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Configuration;

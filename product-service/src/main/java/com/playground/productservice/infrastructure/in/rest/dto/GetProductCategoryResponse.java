97041bf3773152a0bf4749d2a50d6d5c0e316f0b
dongkyunkim <ddkds66@gmail.com>
2023-08-27 22:44:30 +0900

    Response DTO field에 Schema 작성

--- product-service/src/main/java/com/playground/productservice/infrastructure/in/rest/dto/GetProductCategoryResponse.java
+++ product-service/src/main/java/com/playground/productservice/infrastructure/in/rest/dto/GetProductCategoryResponse.java
@@ -1,5 +1,6 @@
 package com.playground.productservice.infrastructure.in.rest.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,8 +12,10 @@ import lombok.NoArgsConstructor;
 @Builder
 public class GetProductCategoryResponse {
 
+    @Schema(description = "상품 카테고리 ID", example = "1")
     private Long productCategoryId;
 
+    @Schema(description = "상품 카테고리명", example = "Clothing")
     private String name;
 
 }

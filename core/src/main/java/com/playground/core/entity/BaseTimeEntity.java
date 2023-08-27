0f2d73b36b96729d20601625e6df7c9b70c09196
dongkyunkim <ddkds66@gmail.com>
2023-08-27 17:55:41 +0900

    Global Time Zone 적용을 위해 jpa audit 필드 type을 ZonedDateTime으로 변경

--- core/src/main/java/com/playground/core/entity/BaseTimeEntity.java
+++ core/src/main/java/com/playground/core/entity/BaseTimeEntity.java
@@ -7,7 +7,7 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
 
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
@@ -15,10 +15,10 @@ public abstract class BaseTimeEntity {
 
     @CreatedDate
     @Column(name = "created_at", nullable = false, updatable = false)
-    private LocalDateTime createdAt;
+    private ZonedDateTime createdAt;
 
     @LastModifiedDate
     @Column(name = "updated_at", nullable = false)
-    private LocalDateTime updatedAt;
+    private ZonedDateTime updatedAt;
 
 }

f8624afd2f46fe54ae2449da10f18b5b0644a57f
dongkyunkim <ddkds66@gmail.com>
2023-08-30 05:59:03 +0900

    Eureka Server 구성

---
+++ discovery-server/src/main/java/com/playground/discoveryserver/DiscoveryServerApplication.java
@@ -0,0 +1,17 @@
+package com.playground.discoveryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = "com.playground")
+@EnableEurekaServer
+public class DiscoveryServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryServerApplication.class, args);
+    }
+
+}

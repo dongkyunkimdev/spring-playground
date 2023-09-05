7b722b4c2f431d10d241f132a6334fd0dd979efd
dongkyunkim <ddkds66@gmail.com>
2023-09-06 07:56:31 +0900

    상품 카테고리 리스트 조회 API의 Parameter들을 DTO @ParameterObject로 변경

--- product-service/src/main/java/com/playground/productservice/infrastructure/in/rest/GetProductCategoryListRestAdapter.java
+++ product-service/src/main/java/com/playground/productservice/infrastructure/in/rest/GetProductCategoryListRestAdapter.java
@@ -5,10 +5,10 @@ import com.playground.core.paging.SliceResponse;
 import com.playground.productservice.application.port.in.usecase.GetProductCategoryListUseCase;
 import com.playground.productservice.application.port.in.usecase.dto.GetProductCategoryInfo;
 import com.playground.productservice.application.port.in.usecase.dto.GetProductCategoryListCommand;
+import com.playground.productservice.infrastructure.in.rest.dto.GetProductCategoryListRequest;
 import com.playground.productservice.infrastructure.in.rest.dto.GetProductCategoryResponse;
 import com.playground.productservice.util.mapper.GetProductCategoryMapper;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springdoc.core.annotations.ParameterObject;
@@ -17,7 +17,6 @@ import org.springframework.data.domain.Slice;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @RestAdapter
@@ -33,12 +32,10 @@ public class GetProductCategoryListRestAdapter {
     @GetMapping("/v1/product/category")
     @ResponseStatus(HttpStatus.OK)
     public SliceResponse<GetProductCategoryResponse> getProductCategory(
-            @Parameter(required = false, description = "상품 카테고리 시작 ID.") @RequestParam(required = false) final Long fromProductCategoryId,
-            @Parameter(required = false, description = "상품 카테고리 종료 ID.") @RequestParam(required = false) final Long toProductCategoryId,
-            @Parameter(required = false, description = "상품 카테고리 이름.") @RequestParam(required = false) final String productCategoryName,
+            @ParameterObject GetProductCategoryListRequest request,
             @ParameterObject @PageableDefault Pageable pageable
     ) {
-        Slice<GetProductCategoryInfo> getProductCategoryInfoSlice = getProductCategoryListUseCase.execute(new GetProductCategoryListCommand(fromProductCategoryId, toProductCategoryId, productCategoryName), pageable);
+        Slice<GetProductCategoryInfo> getProductCategoryInfoSlice = getProductCategoryListUseCase.execute(new GetProductCategoryListCommand(request.getFromProductCategoryId(), request.getToProductCategoryId(), request.getProductCategoryName()), pageable);
 
         return SliceResponse.of(getProductCategoryInfoSlice.map(mapper::infoToResponse));
     }
--- product-service/src/main/java/com/playground/productservice/infrastructure/in/rest/dto/GetProductCategoryListRequest.java
+++ product-service/src/main/java/com/playground/productservice/infrastructure/in/rest/dto/GetProductCategoryListRequest.java
@@ -1,5 +1,6 @@
 package com.playground.productservice.infrastructure.in.rest.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,10 +12,13 @@ import lombok.NoArgsConstructor;
 @Builder
 public class GetProductCategoryListRequest {
 
+    @Schema(description = "상품 카테고리 시작 ID", example = "1")
     private Long fromProductCategoryId;
 
+    @Schema(description = "상품 카테고리 종료 ID", example = "10")
     private Long toProductCategoryId;
 
+    @Schema(description = "상품 카테고리 이름.", example = "Clothing")
     private String productCategoryName;
 
 }

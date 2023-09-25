c8092c19e0b50e44c47ddecd1fb73d9ec278a265
dongkyunkim <ddkds66@gmail.com>
2023-09-26 00:53:23 +0900

    Usage가 없는 MapStruct 메서드 제거

--- product-service/src/main/java/com/playground/productservice/util/mapper/GetProductCategoryMapper.java
+++ product-service/src/main/java/com/playground/productservice/util/mapper/GetProductCategoryMapper.java
@@ -1,6 +1,5 @@
 package com.playground.productservice.util.mapper;
 
-import com.playground.productservice.application.port.in.usecase.dto.GetProductCategoryCommand;
 import com.playground.productservice.application.port.in.usecase.dto.GetProductCategoryInfo;
 import com.playground.productservice.domain.ProductCategory;
 import com.playground.productservice.infrastructure.in.rest.dto.GetProductCategoryResponse;
@@ -9,8 +8,6 @@ import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring", config = UnmappedIgnoreConfig.class)
 public interface GetProductCategoryMapper {
-
-    ProductCategory commandToEntity(GetProductCategoryCommand command);
 
     GetProductCategoryInfo entityToInfo(ProductCategory productCategory);
 

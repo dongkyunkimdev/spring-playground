5005052a721f2ca04dd4c4a615fa1454ab69e3aa
dongkyunkim <ddkds66@gmail.com>
2023-09-27 21:34:04 +0900

    상품 pk가 존재하지 않아 상품 조회에 실패하는 Integration 테스트 케이스 작성

--- product-service/src/test/java/com/playground/productservice/infrastructure/in/rest/GetProductRestAdapterTest.java
+++ product-service/src/test/java/com/playground/productservice/infrastructure/in/rest/GetProductRestAdapterTest.java
@@ -1,7 +1,9 @@
 package com.playground.productservice.infrastructure.in.rest;
 
 import com.fasterxml.jackson.core.type.TypeReference;
+import com.playground.core.exception.dto.ErrorResponse;
 import com.playground.core.exception.dto.SuccessResponse;
+import com.playground.productservice.domain.exception.ProductErrorCode;
 import com.playground.productservice.infrastructure.in.rest.dto.CommonProductCategoryResponse;
 import com.playground.productservice.infrastructure.in.rest.dto.GetProductResponse;
 import com.playground.productservice.support.ControllerTest;
@@ -50,6 +52,28 @@ class GetProductRestAdapterTest extends ControllerTest {
         CommonProductCategoryResponse productCategoryResponse = productResponse.productCategory();
         assertThat(productCategoryResponse.productCategoryId()).isEqualTo(1);
         assertThat(productCategoryResponse.name()).isEqualTo("Clothing");
+    }
+
+    @Test
+    void 상품_조회_실패_상품이_존재하지_않음() throws Exception {
+        // given
+        final Long productId = 1892731L;
+
+        // when
+        MockHttpServletRequestBuilder requestBuilder = createRequestBuilder(HttpMethod.GET, "/v1/product/{productId}", productId);
+
+        ResultActions result = mvc.perform(requestBuilder);
+
+        // then
+        result.andExpect(status().isNotFound());
+
+        ErrorResponse responseDto = getErrorResponse(result);
+        ProductErrorCode errorCode = ProductErrorCode.PRODUCT_NOT_FOUND;
+
+        assertThat(responseDto.isSuccess()).isFalse();
+        assertThat(responseDto.getStatus()).isEqualTo(errorCode.getStatus());
+        assertThat(responseDto.getCode()).isEqualTo(errorCode.getCode());
+        assertThat(responseDto.getReason()).isEqualTo(errorCode.getReason());
     }
 
 }

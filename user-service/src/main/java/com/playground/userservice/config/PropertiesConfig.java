4176899afe3f0cd828186827abced379b196a5c4
dongkyunkim <ddkds66@gmail.com>
2023-10-01 04:31:39 +0900

    ConfigurationProperties 빈 등록을 위한 @ConfigurationPropertiesScan config 구성

---
+++ user-service/src/main/java/com/playground/userservice/config/PropertiesConfig.java
@@ -0,0 +1,10 @@
+package com.playground.userservice.config;
+
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationPropertiesScan(basePackages = "com.playground")
+public class PropertiesConfig {
+
+}

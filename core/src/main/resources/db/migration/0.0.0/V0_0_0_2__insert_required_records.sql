b865fdb4b9615b1dc386492741af7b359647bb9d
dongkyunkim <ddkds66@gmail.com>
2023-10-01 00:54:52 +0900

    local, dev 환경용 admin 계정 flyway 데이터 추가

---
+++ core/src/main/resources/db/migration/0.0.0/V0_0_0_2__insert_required_records.sql
@@ -0,0 +1,6 @@
+use db_user;
+
+-- local, dev
+INSERT INTO `user`(user_id, username, password, nickname)
+VALUES (1, 'admin@playground.com', '$2a$10$Tu7KuI7Ctw90fGwALAoEQuzFT0/2zV7J2mqZeTcV.x3eioMw7rtDC', 'admin')
+;

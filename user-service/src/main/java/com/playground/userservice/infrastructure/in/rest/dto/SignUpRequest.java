bc734cd2499c93f45547e81df391738482a8b3f1
dongkyunkim <ddkds66@gmail.com>
2023-10-01 04:49:35 +0900

    불필요 주석 제거

--- user-service/src/main/java/com/playground/userservice/infrastructure/in/rest/dto/SignUpRequest.java
+++ user-service/src/main/java/com/playground/userservice/infrastructure/in/rest/dto/SignUpRequest.java
@@ -8,7 +8,6 @@ import jakarta.validation.constraints.Size;
 public record SignUpRequest(
     @NotBlank(message = "username은 비어있을 수 없습니다.")
     @Email(message = "이메일 형식만 입력할 수 있습니다.")
-//    @Length()
     @Size(max = 50, message = "username은 최대 50글자까지 입력할 수 있습니다.")
     @Schema(description = "로그인 아이디", example = "ddkds66@gmail.com") String username,
 

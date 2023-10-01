a5beeb46fc5c21d50691146e47d5b5e37c5579bb
dongkyunkim <ddkds66@gmail.com>
2023-10-02 00:53:54 +0900

    로그인 integration 테스트 케이스 작성

--- core/src/main/resources/db/migration/0.0.0/V0_0_0_2__insert_required_records.sql
+++ core/src/main/resources/db/migration/0.0.0/V0_0_0_2__insert_required_records.sql
@@ -1,6 +1,10 @@
-use db_user;
+use
+db_user;
 
 -- local, dev
 INSERT INTO `user`(user_id, username, password, nickname, status, role, last_login_at)
-VALUES (1, 'admin@playground.com', '$2a$10$Tu7KuI7Ctw90fGwALAoEQuzFT0/2zV7J2mqZeTcV.x3eioMw7rtDC', 'admin', 'ACTIVE', 'ADMIN', '2023-10-01 23:54:15')
+VALUES (1, 'admin@playground.com', '$2a$10$Tu7KuI7Ctw90fGwALAoEQuzFT0/2zV7J2mqZeTcV.x3eioMw7rtDC', 'admin', 'ACTIVE', 'ADMIN', '2023-10-01 23:54:15'),
+       (2, 'inactive@playground.com', '$2a$10$Tu7KuI7Ctw90fGwALAoEQuzFT0/2zV7J2mqZeTcV.x3eioMw7rtDC', 'admin', 'INACTIVE', 'USER', '2023-10-01 23:54:15'),
+       (3, 'deleted@playground.com', '$2a$10$Tu7KuI7Ctw90fGwALAoEQuzFT0/2zV7J2mqZeTcV.x3eioMw7rtDC', 'admin', 'DELETED', 'USER', '2023-10-01 23:54:15'),
+       (4, 'blocked@playground.com', '$2a$10$Tu7KuI7Ctw90fGwALAoEQuzFT0/2zV7J2mqZeTcV.x3eioMw7rtDC', 'admin', 'BLOCKED', 'USER', '2023-10-01 23:54:15')
 ;
---
+++ user-service/src/test/java/com/playground/userservice/infrastructure/in/rest/SignInRestAdapterTest.java
@@ -0,0 +1,141 @@
+package com.playground.userservice.infrastructure.in.rest;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.playground.core.exception.dto.ErrorResponse;
+import com.playground.core.exception.dto.SuccessResponse;
+import com.playground.userservice.domain.exception.UserErrorCode;
+import com.playground.userservice.infrastructure.in.rest.dto.SignInRequest;
+import com.playground.userservice.infrastructure.in.rest.dto.SignInResponse;
+import com.playground.userservice.support.ControllerTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.transaction.annotation.Transactional;
+
+import static com.playground.userservice.support.AssertUtil.assertErrorResponse;
+import static com.playground.userservice.support.AssertUtil.assertSuccessResponse;
+import static com.playground.userservice.support.ControllerTestUtil.createRequestBuilder;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@Transactional
+@ActiveProfiles("local")
+class SignInRestAdapterTest extends ControllerTest {
+
+    @Test
+    void 로그인_성공() throws Exception {
+        // given
+        final SignInRequest requestDto = new SignInRequest("admin@playground.com", "1234");
+
+        // when
+        MockHttpServletRequestBuilder requestBuilder = createRequestBuilder(HttpMethod.POST, "/v1/users/signin")
+            .content(objectMapper.writeValueAsString(requestDto));
+
+        ResultActions result = mvc.perform(requestBuilder);
+
+        // then
+        result.andExpect(status().isOk());
+
+        SuccessResponse responseDto = getSuccessResponse(result);
+        assertSuccessResponse(responseDto, HttpStatus.OK);
+
+        SignInResponse signInResponse = objectMapper.convertValue(responseDto.getData(), new TypeReference<>() {
+        });
+        assertThat(signInResponse.accessToken()).isNotNull();
+        assertThat(signInResponse.refreshToken()).isNotNull();
+    }
+
+    @Test
+    void 로그인_실패_유저가_존재하지_않음() throws Exception {
+        // given
+        final SignInRequest requestDto = new SignInRequest("notExist@gmail.com", "1234");
+
+        // when
+        MockHttpServletRequestBuilder requestBuilder = createRequestBuilder(HttpMethod.POST, "/v1/users/signin")
+            .content(objectMapper.writeValueAsString(requestDto));
+
+        ResultActions result = mvc.perform(requestBuilder);
+
+        // then
+        result.andExpect(status().isNotFound());
+
+        ErrorResponse responseDto = getErrorResponse(result);
+        assertErrorResponse(responseDto, UserErrorCode.USER_NOT_FOUND);
+    }
+
+    @Test
+    void 로그인_실패_비밀번호가_일치하지_않음() throws Exception {
+        // given
+        final SignInRequest requestDto = new SignInRequest("admin@playground.com", "12341234");
+
+        // when
+        MockHttpServletRequestBuilder requestBuilder = createRequestBuilder(HttpMethod.POST, "/v1/users/signin")
+            .content(objectMapper.writeValueAsString(requestDto));
+
+        ResultActions result = mvc.perform(requestBuilder);
+
+        // then
+        result.andExpect(status().isUnauthorized());
+
+        ErrorResponse responseDto = getErrorResponse(result);
+        assertErrorResponse(responseDto, UserErrorCode.PASSWORD_MISMATCH);
+    }
+
+    @Test
+    void 로그인_실패_비활성화된_유저() throws Exception {
+        // given
+        final SignInRequest requestDto = new SignInRequest("inactive@playground.com", "1234");
+
+        // when
+        MockHttpServletRequestBuilder requestBuilder = createRequestBuilder(HttpMethod.POST, "/v1/users/signin")
+            .content(objectMapper.writeValueAsString(requestDto));
+
+        ResultActions result = mvc.perform(requestBuilder);
+
+        // then
+        result.andExpect(status().isForbidden());
+
+        ErrorResponse responseDto = getErrorResponse(result);
+        assertErrorResponse(responseDto, UserErrorCode.USER_INACTIVE);
+    }
+
+    @Test
+    void 로그인_실패_삭제된_유저() throws Exception {
+        // given
+        final SignInRequest requestDto = new SignInRequest("deleted@playground.com", "1234");
+
+        // when
+        MockHttpServletRequestBuilder requestBuilder = createRequestBuilder(HttpMethod.POST, "/v1/users/signin")
+            .content(objectMapper.writeValueAsString(requestDto));
+
+        ResultActions result = mvc.perform(requestBuilder);
+
+        // then
+        result.andExpect(status().isForbidden());
+
+        ErrorResponse responseDto = getErrorResponse(result);
+        assertErrorResponse(responseDto, UserErrorCode.USER_DELETED);
+    }
+
+    @Test
+    void 로그인_실패_블락된_유저() throws Exception {
+        // given
+        final SignInRequest requestDto = new SignInRequest("blocked@playground.com", "1234");
+
+        // when
+        MockHttpServletRequestBuilder requestBuilder = createRequestBuilder(HttpMethod.POST, "/v1/users/signin")
+            .content(objectMapper.writeValueAsString(requestDto));
+
+        ResultActions result = mvc.perform(requestBuilder);
+
+        // then
+        result.andExpect(status().isForbidden());
+
+        ErrorResponse responseDto = getErrorResponse(result);
+        assertErrorResponse(responseDto, UserErrorCode.USER_BLOCKED);
+    }
+
+}
